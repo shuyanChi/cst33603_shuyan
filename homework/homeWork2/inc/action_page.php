f82758eac840dd77c9153f575d6fc038af4441d7
shuyan <schi@csumb.edu>
2018-02-26 07:23:16 +0000

    add hw2

---
+++ homework/homeWork2/inc/action_page.php
@@ -0,0 +1,51 @@
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+
+<?php
+session_start();
+// define variables and set to empty values
+$name = $email = $gender= $password= $comment = $website = $color= $possession ="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $password = test_input($_POST["password"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+  $color = test_input($_POST["color"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<?php
+if(!empty($_POST['name'] && $_POST['password' ] && $_POST['email'] && $_POST['website'] && $_POST['comment'] && $_POST['gender'])){
+    echo "<h2>Info:</h2>";
+    echo "Your name is: " . $name;
+    echo "<br>";
+    echo "Your password is: " . $password;
+    echo"<br>";
+    echo "Your email is: " . $email;
+    echo "<br>";
+    echo "Your website is: " . $website;
+    echo "<br>";
+    echo "Your comment is: " . $comment;
+    echo "<br>";
+    echo "Your gender is: " . $gender;
+    echo "<br>";
+    echo "This hex is the color you like: " .$color;
+    echo "<br>";
+}
+
+?>
+
+<?php 
+echo "You have: " . $_POST["possession"]; 
+?><br>
+
+
